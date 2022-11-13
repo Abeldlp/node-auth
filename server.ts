55d8a7005d686476395e7ce01ea3c2a3a8ea520d
Abel de la Paz <abel45991690@gmail.com>
2022-11-13 20:20:28 +0100

    chore: update main route endpoints

--- server.ts
+++ server.ts
@@ -5,12 +5,13 @@ import { authRouter } from "./routes/auth.js";
 import { userRouter } from "./routes/user.js";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/", authRouter);
 app.use("/users", userRouter);
-app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
